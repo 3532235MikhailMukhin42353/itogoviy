8c2d2319eb7776e9d5042b470ac4d09549d6d3b4
3532235MikhailMukhin42353 <dzhon.seryy@mail.ru>
2023-03-13 15:49:57 +0300

    itogoviy :wq

---
+++ b1/Program.cs
@@ -0,0 +1,29 @@
+﻿/*Из имеющегося массива строк формирует массив из строк, длина которых меньше либо равна 3 символа. Первоначальный массив 
+можно ввести с клавиатуры.*/
+
+string [] Array = new string [6] {"hello", "231", "mi", "good", "a23", "a"};
+string [] NewArray = new string [Array.Length];
+FillArray(Array, NewArray);
+PrintArray(NewArray);
+
+void FillArray (string [] Array, string [] NewArray)
+{
+    //int count = 0;
+    for (int i = 0; i < Array.Length; i++)
+    {
+        if(Array[i].Length <= 3)
+        NewArray[i] = Array[i];
+        //NewArray[count] = Array[i];
+        //count++;
+    }
+}
+
+void PrintArray (string [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine($"{array[i]}");
+    }
+    Console.WriteLine();
+}
+
